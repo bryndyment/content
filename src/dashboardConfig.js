8b371208646aca8c618c2bc854af4d10029cddc6
Bryn Dyment <bryndyment@gmail.com>
2020-05-04 11:47:45 +0900

    desktop deploy

---
+++ src/dashboardConfig.js
@@ -0,0 +1,17 @@
+export default {
+  widgets: [
+      {
+      name: 'netlify',
+      options: {
+        sites: [
+          {
+            apiId: '1ab3afc1-1e76-419f-b3f5-f72f031f8666',
+            buildHookId: '5eaf81d8e83a65e4d288c5ed',
+            name: 'practical-mccarthy-ee95bf',
+            title: 'i.k.s'
+          }
+        ]
+      }
+    }
+  ]
+}
