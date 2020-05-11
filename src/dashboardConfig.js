21a172754dbfc232906a8c621fcddc6b1dc07d37
Bryn Dyment <bryndyment@gmail.com>
2020-05-11 22:15:42 +0900

    deploy-related

--- src/dashboardConfig.js
+++ src/dashboardConfig.js
@@ -5,10 +5,16 @@ export default {
       options: {
         sites: [
           {
+            apiId: 'c98a7607-0472-4133-aae7-5b4e300caa54',
+            buildHookId: '5eaf68fc50cfcd458a3451a9',
+            name: 'hoologic',
+            title: 'HOO LOGIC'
+          },
+          {
             apiId: '1ab3afc1-1e76-419f-b3f5-f72f031f8666',
             buildHookId: '5eaf81d8e83a65e4d288c5ed',
             name: 'practical-mccarthy-ee95bf',
-            title: 'i.k.s'
+            title: 'i.k.s design'
           }
         ]
       }
