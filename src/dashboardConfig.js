4b4c81fd003ae4b4f7cb71ef08124afb617c1fb2
Bryn Dyment <bryndyment@gmail.com>
2020-10-07 09:29:47 +0900

    remove iksdesign

--- src/dashboardConfig.js
+++ src/dashboardConfig.js
@@ -9,12 +9,6 @@ export default {
             buildHookId: '5f7d098bf7edec18fd063e68',
             name: 'hoologic',
             title: 'HOO LOGIC'
-          },
-          {
-            apiId: 'f986c359-0c78-4afb-a17d-c06cc8045741',
-            buildHookId: '5f7d09eb363deb1b3e1f3d1c',
-            name: 'iksdesign',
-            title: 'i.k.s design'
           }
         ]
       }
