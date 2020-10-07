15a223a7ba0089a3a2a09cee4f36fd321935be86
Bryn Dyment <bryndyment@gmail.com>
2020-10-07 09:21:26 +0900

    new build hooks

--- src/dashboardConfig.js
+++ src/dashboardConfig.js
@@ -6,13 +6,13 @@ export default {
         sites: [
           {
             apiId: 'c98a7607-0472-4133-aae7-5b4e300caa54',
-            buildHookId: '5eaf68fc50cfcd458a3451a9',
+            buildHookId: '5f7d098bf7edec18fd063e68',
             name: 'hoologic',
             title: 'HOO LOGIC'
           },
           {
             apiId: 'f986c359-0c78-4afb-a17d-c06cc8045741',
-            buildHookId: '5eca3c4640c1623eb75debed',
+            buildHookId: '5f7d09eb363deb1b3e1f3d1c',
             name: 'iksdesign',
             title: 'i.k.s design'
           }
